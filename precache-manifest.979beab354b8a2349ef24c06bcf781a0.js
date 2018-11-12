f3c4cc19ec3b76584eb3d75e7829725652b6ce22
tintinve <pedroluismejia@gmail.com>
2018-11-12 10:33:02 +0100

    V0.1

---
+++ precache-manifest.979beab354b8a2349ef24c06bcf781a0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d6b2f7af507b97fd6d28",
+    "url": "/static/js/main.d6b2f7af.chunk.js"
+  },
+  {
+    "revision": "fdc611c83da4f5c58f49",
+    "url": "/static/js/1.fdc611c8.chunk.js"
+  },
+  {
+    "revision": "9dabc3f1893ca12fcda4bc67c2c0704e",
+    "url": "/index.html"
+  }
+];
